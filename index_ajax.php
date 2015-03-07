c81bb5df06f80856318dfc6526d4d01722631bef
Nelly <Nelly@192.168.1.67>
2015-03-07 16:53:17 -0600

    Formateo de texto con el IDE.

--- index_ajax.php
+++ index_ajax.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'clases/UtilDB.php';
 if (isset($_POST['xAccion'])) {
     if ($_POST['xAccion'] == "getGrados") {
@@ -14,7 +15,7 @@ if (isset($_POST['xAccion'])) {
             $tmp .= "<a href=\"#\" class=\"list-group-item\">&nbsp;</a>";
 
             foreach ($rst as $row) {
-                $tmp .= "<a href=\"javascript:void(0);\" class=\"list-group-item\" onclick=\"getClasificacionesProductos($cveRito,$cveClasificacion,".$row['cve_grado'].",'".$row['descripcion']."');\">" . $row['descripcion'] . "</a>";
+                $tmp .= "<a href=\"javascript:void(0);\" class=\"list-group-item\" onclick=\"getClasificacionesProductos($cveRito,$cveClasificacion," . $row['cve_grado'] . ",'" . $row['descripcion'] . "');\">" . $row['descripcion'] . "</a>";
             }
             $rst->closeCursor();
             $tmp .= "</div>";
@@ -23,29 +24,23 @@ if (isset($_POST['xAccion'])) {
             echo($tmp);
         }
     }
-    
-    if ($_POST['xAccion'] == "getClasificacionProductos")
-    {
-         if (isset($_POST['cveRito']) && isset($_POST['cveClasificacion']) && isset($_POST['cveGrado']) && isset($_POST['nombreGrado']))
-         {
+
+    if ($_POST['xAccion'] == "getClasificacionProductos") {
+        if (isset($_POST['cveRito']) && isset($_POST['cveClasificacion']) && isset($_POST['cveGrado']) && isset($_POST['nombreGrado'])) {
             $cveRito2 = $_POST['cveRito'];
             $cveClasificacion2 = $_POST['cveClasificacion'];
             $cveGrado2 = $_POST['cveGrado'];
             $nombreGrado2 = $_POST['nombreGrado'];
-            $tmp2 ="<div class=\"row\"><div class=\"col-md-12\"><h1>$nombreGrado2</h1></div></div>";
-            
+            $tmp2 = "<div class=\"row\"><div class=\"col-md-12\"><h1>$nombreGrado2</h1></div></div>";
+
             $sql2 = "SELECT * FROM clasificaciones_productos WHERE cve_rito = $cveRito2 AND cve_clasificacion= $cveClasificacion2 AND cve_grado=$cveGrado2";
             $rst2 = UtilDB::ejecutaConsulta($sql2);
-            foreach ($rst2 as $row2)
-            {
-                $tmp2 .="<div class=\"row\"><div class=\"col-md-12\"><h2>".$row2['descripcion']."</h2></div></div>";
+            foreach ($rst2 as $row2) {
+                $tmp2 .="<div class=\"row\"><div class=\"col-md-12\"><h2>" . $row2['descripcion'] . "</h2></div></div>";
             }
             $rst2->closeCursor();
             echo($tmp2);
-            
-         }
-        
+        }
     }
 }
 ?>
-
