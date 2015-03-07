8f921cd3e7fac84989e7ecac52f25e5e9bef85c9
Nelly <Nelly@192.168.1.67>
2015-03-07 17:18:50 -0600

    Se sustituye por el uso de jQuery y Bootstrap 3 en modo local, se cambio:
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
    
    Por este otro codigo:
    
            <link href="../twbs/bootstrap-3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
            <script src="../js/jQuery/jquery-1.11.2.min.js"></script>
            <script src="../twbs/bootstrap-3.3.2/js/bootstrap.min.js"></script>

--- php/cat_grados.php
+++ php/cat_grados.php
@@ -37,9 +37,9 @@ if (isset($_POST['xAccion'])) {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <link href="../twbs/bootstrap-3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+        <script src="../js/jQuery/jquery-1.11.2.min.js"></script>
+        <script src="../twbs/bootstrap-3.3.2/js/bootstrap.min.js"></script>
         <title>Catálogo de clasificaciónes</title>
         <script>
             
